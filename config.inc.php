82ef43701b00bb67fa0507f8595c27ac55b17db7
mykl98 <michael.skooltech@gmail.com>
2025-07-02 15:47:59 +0800

    1

--- config.inc.php
+++ config.inc.php
@@ -30,8 +30,6 @@ $cfg['Servers'][$i]['auth_type'] = 'cookie';
 $cfg['Servers'][$i]['host'] = '127.0.0.1';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = true;
-$cfg['ExecTimeLimit'] = 600;
-$cfg['MaxSize'] = 256 * 1025 * 1024 * 1024; 
 
 /**
  * phpMyAdmin configuration storage settings.
